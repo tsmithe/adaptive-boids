b80f23c168c0017b04c91f9c88acb169dc8c4a9e
Toby Smithe <git@tsmithe.net>
2014-12-03 21:07:56 +0100

    Let visualise.py see run.py params

--- run.py
+++ run.py
@@ -14,9 +14,9 @@ from statistics import StatisticsHelper
 SEED = 0
 DT = 1
 RUN_TIME = 10000
-DUMP_STATS_INTERVAL = 1
+DUMP_STATS_INTERVAL = 2
 
-WORLD_SIZE = 10
+WORLD_SIZE = 100
 NUM_PREY = 200
 NUM_PREDATORS = 20
 PREY_RADIUS = 5
@@ -24,60 +24,64 @@ PREDATOR_RADIUS = 5
 FEEDING_AREA_RADIUS = 5
 FEEDING_AREA_POSITION = (50, 50)
 
-np.random.seed(SEED)
-
-ecosystem = Ecosystem(WORLD_SIZE, NUM_PREY, NUM_PREDATORS,
-                      PREY_RADIUS, PREDATOR_RADIUS,
-                      FEEDING_AREA_RADIUS, FEEDING_AREA_POSITION, DT)
-
-prey_statistics = StatisticsHelper('prey_', False,
-                                   True, True, True)
-predator_statistics = StatisticsHelper('predator_', False,
-                                       True, True, True)
-
-def export_stats(ecosystem):
+def export_stats(prey_statistics, predator_statistics, ecosystem):
     prey_statistics.update_data(ecosystem.prey, ecosystem.prey_tree)
     prey_statistics.export()
     predator_statistics.update_data(ecosystem.predators,
                                     ecosystem.predator_tree)
     predator_statistics.export()
-    
 
-t = 0
-while t < RUN_TIME:
-    sys.stdout.write("\rt = %.1f" % t)
-    sys.stdout.flush()
-    
-    # Collision check
-    #  -- predators collided with prey are marked as feeding
-    #  -- prey collided with feeding area marked as feeding
-    #  -- collisions induce velocity change
 
-    # Feeding
-    #  -- those boids marked as feeding receive a linear(?) increase in stamina
-    ecosystem.update_stamina()
+def main():    
+    np.random.seed(SEED)
 
-    # Kill and spawn
-    #  -- check for boids marked as dead, and respawn them
-    #  -- update lifespan values for living boids
-    ecosystem.kill_prey()
-    ecosystem.update_age()
-    
-    # Compute statistics and dump data to disk
-    if not t % DUMP_STATS_INTERVAL:
-        export_stats(ecosystem)
+    ecosystem = Ecosystem(WORLD_SIZE, NUM_PREY, NUM_PREDATORS,
+                          PREY_RADIUS, PREDATOR_RADIUS,
+                          FEEDING_AREA_RADIUS, FEEDING_AREA_POSITION, DT)
 
-    # Escape criterion: if avg life span not increasing for large no. of runs
+    prey_statistics = StatisticsHelper('prey_', False,
+                                       True, True, True)
+    predator_statistics = StatisticsHelper('predator_', False,
+                                           True, True, True)
 
-    # Update time
-    t += DT
+    t = 0
+    while t < RUN_TIME:
+        sys.stdout.write("\rt = %.1f" % t)
+        sys.stdout.flush()
 
-    # Update positions (and corresponding regions) and stamina
-    ecosystem.update_boid_velocities()
-    ecosystem.update_velocity_data()
-    ecosystem.update_boid_positions()
-    ecosystem.update_position_data()
+        # Collision check
+        #  -- predators collided with prey are marked as feeding
+        #  -- prey collided with feeding area marked as feeding
+        #  -- collisions induce velocity change
 
-    # ...
+        # Feeding
+        #  -- those boids marked as feeding receive a linear(?) increase in stamina
+        ecosystem.update_stamina()
 
-sys.stdout.write("\n")
+        # Kill and spawn
+        #  -- check for boids marked as dead, and respawn them
+        #  -- update lifespan values for living boids
+        ecosystem.kill_prey()
+        ecosystem.update_age()
+
+        # Compute statistics and dump data to disk
+        if not t % DUMP_STATS_INTERVAL:
+            export_stats(prey_statistics, predator_statistics, ecosystem)
+
+        # Escape criterion: if avg life span not increasing for large no. of runs
+
+        # Update time
+        t += DT
+
+        # Update positions (and corresponding regions) and stamina
+        ecosystem.update_boid_velocities()
+        ecosystem.update_velocity_data()
+        ecosystem.update_boid_positions()
+        ecosystem.update_position_data()
+
+        # ...
+
+    sys.stdout.write("\n")
+
+if __name__ == '__main__':
+    main()    
--- visualise.py
+++ visualise.py
@@ -12,13 +12,19 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import csv, numpy as np
 
-PLOT_MINIMUM = -400
-PLOT_MAXIMUM = 400
+from run import DUMP_STATS_INTERVAL, WORLD_SIZE
 
-def animate(i, prey_graph, predator_graph, prey_frames, predator_frames):
+PLOT_MINIMUM = -2*WORLD_SIZE
+PLOT_MAXIMUM = 2*WORLD_SIZE
+
+FRAME_INTERVAL = 50 # NB: Matplotlib doesn't seem to go much faster...
+
+def animate(i, fig, ax, text, prey_graph, predator_graph,
+            prey_frames, predator_frames):
+    text.set_text("t = %d" % (i*DUMP_STATS_INTERVAL))
     prey_graph.set_offsets(prey_frames[i])
     predator_graph.set_offsets(predator_frames[i])
-    return [prey_graph, predator_graph]
+    return [text, prey_graph, predator_graph]
 
 def collect_data(csv_reader):
     frame_data = []
@@ -36,6 +42,7 @@ prey_reader = csv.reader(open('prey_positions.csv'))
 predator_reader = csv.reader(open('predator_positions.csv'))
 
 fig, ax = plt.subplots()
+text = ax.text(PLOT_MINIMUM+5, PLOT_MAXIMUM-20, "", withdash=True, fontsize=12)
 prey_graph = ax.scatter(100*PLOT_MINIMUM, 100*PLOT_MAXIMUM, color='green')
 predator_graph = ax.scatter(100*PLOT_MINIMUM, 100*PLOT_MAXIMUM, color='red')
 ax.set_xlim(PLOT_MINIMUM, PLOT_MAXIMUM)
@@ -45,9 +52,10 @@ prey_frame_data = collect_data(prey_reader)
 predator_frame_data = collect_data(predator_reader)
 
 ani = animation.FuncAnimation(fig, animate, len(prey_frame_data),
-                              fargs=(prey_graph, predator_graph,
+                              fargs=(fig, ax, text,
+                                     prey_graph, predator_graph,
                                      prey_frame_data, predator_frame_data),
-                              interval=100,
+                              interval=FRAME_INTERVAL,
                               repeat=True)
 plt.show()
 
