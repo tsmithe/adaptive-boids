98eead4142491c19358027741129102291739ba7
Toby Smithe <git@tsmithe.net>
2014-12-05 11:14:30 +0100

    Add boundary and prey/predator sizes to visualisation

--- visualise.py
+++ visualise.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import csv, numpy as np
 
-from run import DUMP_STATS_INTERVAL, DT, WORLD_RADIUS
+from run import DUMP_STATS_INTERVAL, DT, WORLD_RADIUS, PREY_RADIUS, PREDATOR_RADIUS
 
 PLOT_MINIMUM = -2*WORLD_RADIUS
 PLOT_MAXIMUM = 2*WORLD_RADIUS
@@ -42,9 +42,14 @@ prey_reader = csv.reader(open('prey_positions.csv'))
 predator_reader = csv.reader(open('predator_positions.csv'))
 
 fig, ax = plt.subplots()
+boundary = plt.Circle((0, 0), WORLD_RADIUS, facecolor='none',
+                      linestyle='dashed')
+ax.add_artist(boundary)
 text = ax.text(PLOT_MINIMUM+5, PLOT_MAXIMUM-20, "", withdash=True, fontsize=12)
-prey_graph = ax.scatter(100*PLOT_MINIMUM, 100*PLOT_MAXIMUM, color='green')
-predator_graph = ax.scatter(100*PLOT_MINIMUM, 100*PLOT_MAXIMUM, color='red')
+prey_graph = ax.scatter(100*PLOT_MINIMUM, 100*PLOT_MAXIMUM,
+                        np.pi*PREY_RADIUS**2, color='green')
+predator_graph = ax.scatter(100*PLOT_MINIMUM, 100*PLOT_MAXIMUM,
+                            np.pi*PREDATOR_RADIUS**2, color='red')
 ax.set_xlim(PLOT_MINIMUM, PLOT_MAXIMUM)
 ax.set_ylim(PLOT_MINIMUM, PLOT_MAXIMUM)
 
