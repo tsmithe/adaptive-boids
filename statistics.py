c88d8d7e6f36de1fc4546b6f6c76e185f0972a7b
rikardvinge <rik.vinge@gmail.com>
2014-12-11 18:38:49 +0100

    Bug fix in mean nn distance and angular cosine changes

--- statistics.py
+++ statistics.py
@@ -44,6 +44,8 @@ class StatisticsHelper:
     def update_data(self, boids, tree):
         self.boids = boids
         self.tree = tree
+        self.self_indices, self.neighbour_indices = self.find_nearest_neighbour()
+    
 
     def export(self):
         if self.positions_csv:
@@ -71,7 +73,21 @@ class StatisticsHelper:
     @property
     def average_nearest_neighbour(self):
         # Closest neighbour
-        distances, indices = self.tree.query([b.position for b in self.boids])
+        distances_temp, indices_temp = self.tree.query([b.position for b in self.boids], k=2)
+        distances_temp = np.array(distances_temp)
+        distances = np.zeros(np.size(distances_temp)/2)
+
+        for i in np.arange(np.size(distances_temp)/2):
+            if (distances_temp[i,0] == 0.0):
+                distances[i] = distances_temp[i,1]
+            elif (distances_temp[i,1] == 0.0):
+                distances[i] = distances_temp[i,0]
+            else:
+                if (distances_temp[i,0] < distances_temp[i,1]):
+                    distances[i] = distances_temp[i,0]
+                else:
+                    distances[i] = distances_temp[i,1]
+
         return np.mean(distances)
     
     @property
@@ -98,7 +114,48 @@ class StatisticsHelper:
         # Implements the measure described here: http://en.wikipedia.org/wiki/Mean_of_circular_quantities#Mean_of_angles
         # 0 means the velocity vectors point in randomly distributed directions
         # 1 means the velocity vectors are coordinated in the same direction
+        
         mean = np.array([0,0])
+        boid_velocities = np.array([b.velocity for b in self.boids])
+        angular_cosine = np.zeros(np.size(boid_velocities)/2)
+
+        for i in np.arange(np.size(angular_cosine)):
+            self_velocity = boid_velocities[self.self_indices[i],:]
+            self_direction = self_velocity/np.linalg.norm(self_velocity)
+            neighbour_velocity = boid_velocities[self.neighbour_indices[i],:]
+            neighbour_direction = neighbour_velocity/np.linalg.norm(neighbour_velocity)
+            angular_cosine[i] = np.dot(self_direction, neighbour_direction)
+        """
         for b in self.boids:
+            self_velocity = boid_velocities[
             mean = np.add(mean,b.velocity/np.linalg.norm(b.velocity))
         return np.linalg.norm(mean/len(self.boids))
+        """
+        return np.mean(angular_cosine)
+
+    def find_nearest_neighbour(self):
+        distances, indices = self.tree.query([b.position for b in self.boids], k=2)
+        self_indices = []
+        neighbour_indices = []
+
+        for i in np.arange(np.size(indices)/2):
+            if (distances[i,0] == 0.0):
+                self_indices.append(indices[i,0])
+                neighbour_indices.append(indices[i,1])
+            elif (distances[i,1] == 0.0):
+                self_indices.append(indices[i,1])
+                neighbour_indices.append(indices[i,1])
+            else:
+                if (distances[i,0] < distances[i,1]):
+                    self_indices.append(indices[i,1])
+                    neighbour_indices.append(indices[i,0])
+                else:
+                    self_indices.append(indices[i,0])
+                    neighbour_indices.append(indices[i,1])
+        
+        return self_indices, neighbour_indices
+
+
+
+
+
