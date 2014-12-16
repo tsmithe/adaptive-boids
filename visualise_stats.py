bb4bc4dd898390197e0e5650a72f760ee05f5b1d
Toby Smithe <git@tsmithe.net>
2014-12-16 09:11:14 +0100

    Squashed commit of the following:
    commit f736da18a82cbd3460ca1c02da135c2ed7b19a62
    Author: rikardvinge <rik.vinge@gmail.com>
    Date:   Mon Dec 15 23:57:33 2014 +0100
    
        Added plotting functionality
    
        Functionality to calculate moving average and standard deviation

--- visualise_stats.py
+++ visualise_stats.py
@@ -19,6 +19,8 @@ else:
 
 data_dir = config['DEFAULT']['data_dir']
 csv_reader = csv.reader(open(os.path.join(data_dir, 'prey_scalars.csv')))
+dt = eval(config['DEFAULT']['dt'])
+dump_stats_interval = eval(config['DEFAULT']['dump_stats_interval'])
 
 def collect_data(csv_reader):
     frame_data = []
@@ -27,15 +29,79 @@ def collect_data(csv_reader):
         frame_data.append(np.array(list(map(lambda x: float(x), row))))
         
     return np.transpose(frame_data)
+    
+def time_vector(data_vector, dt, dump_stats_interval):
+    time_between_dumps = dt*dump_stats_interval
+    time_vector = time_between_dumps*np.arange(len(data_vector))
+    return time_vector
+
+def calculate_moving_average(data_vector, n):
+    # Half point to the left, half to the right
+    n_half = np.floor(n-1)
+    moving_average = np.zeros(len(data_vector))
+    for i in np.arange(len(data_vector)):
+        if (i-n_half < 0):
+            moving_average[i] = np.mean(data_vector[0:i+n_half])
+        elif (i+n_half > len(data_vector)):
+            moving_average[i] = np.mean(data_vector[i-n_half:])
+        else:
+            moving_average[i] = np.mean(data_vector[i-n_half:i+n_half])
+    return moving_average
+    
+def calculate_moving_std(data_vector, n):
+    n_half = np.floor(n-1)
+    moving_std = np.zeros(len(data_vector))
+    for i in np.arange(len(data_vector)):
+        if (i-n_half < 0):
+            moving_std[i] = np.std(data_vector[0:i+n_half])
+        elif (i+n_half > len(data_vector)):
+            moving_std[i] = np.std(data_vector[i-n_half:])
+        else:
+            moving_std[i] = np.std(data_vector[i-n_half:i+n_half])
+    return moving_std    
 
 data = collect_data(csv_reader)
 
-plt.plot(data[0])
+# Time vector
+time_vector = time_vector(data[0], dt, dump_stats_interval)
+
+# Moving average and std of nearest-neighbour distance, keep n odd
+nn_dist_moving_avg = calculate_moving_average(data[0],501)
+#nn_dist_moving_std = calculate_moving_std(data[0],501)
+
+# Moving average and std of nearest-neighbour angular deviation, keep n odd
+ang_dev_moving_avg = calculate_moving_average(data[2],501)
+#ang_dev_moving_std = calculate_moving_std(data[2],501)
+
+plt.plot(time_vector, data[0])
 plt.title("Average distance to nearest neighbour")
+plt.xlabel("Time")
+plt.ylabel("Average distance")
+
 plt.figure()
-plt.plot(data[1])
+plt.plot(time_vector, nn_dist_moving_avg)
+plt.title("Average distance to nearest neighbour, moving average of nearest 500 points")
+plt.xlabel("Time")
+plt.ylabel("Average distance")
+
+plt.figure()
+plt.plot(time_vector, data[1])
 plt.title("Average distance to centre of mass")
+plt.xlabel("Time")
+
 plt.figure()
-plt.title("Angular deviation")
-plt.plot(data[2])
+plt.plot(time_vector, data[2])
+plt.title("Cosine of angular deviation")
+plt.xlabel("Time")
+plt.ylabel("Angular deviation")
+
+plt.figure()
+plt.plot(time_vector, ang_dev_moving_avg)
+plt.title("Cosine of angular deviation, moving averaging of the nearest 500 points")
+plt.xlabel("Time")
+plt.ylabel("Angular deviation")
+
+plt.figure()
+plt.plot(time_vector, nn_dist_moving_avg/np.max(nn_dist_moving_avg), time_vector, ang_dev_moving_avg)
+
 plt.show()
